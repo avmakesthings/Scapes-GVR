3ac875feca08422caceafbef18a1c0e6d1f05e03
AV <av@anastasia.io>
2017-06-12 18:26:59 -0700

    updated intro scene, added audio visualization for sphere scene

---
+++ Assets/_Scripts/AudioParticles.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AudioParticles : MonoBehaviour {
+	
+	Transform particleTransform;
+	public MeshFilter meshBounds;
+
+	public ParticleSystem myParticle;
+	Mesh mesh;
+
+	Vector3 controlGeoCenter;
+	Vector3 randomVertex;
+	Vector3 randomPointAB;
+
+	Vector3 currentPoint;
+	
+	ParticleSystem a ;
+
+	void Awake()
+	{
+		currentPoint = new Vector3(0,0,10);
+		a = (ParticleSystem)Instantiate(myParticle, currentPoint, Quaternion.identity );
+		a.transform.parent = gameObject.transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		mesh = meshBounds.mesh;
+		controlGeoCenter = meshBounds.transform.position;
+		currentPoint = a.transform.position;
+		findNextPoint();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Update base position of transform 
+		
+		float audioInput = AudioPeer.audioBandBuffer[0];
+		
+
+		if(audioInput>=0.7f){
+			assignPoint();
+			findNextPoint();
+			
+		}		
+	}
+
+	
+	void findNextPoint(){
+
+		randomVertex = mesh.vertices[Random.Range(0,mesh.vertexCount)]; 
+		randomPointAB = Vector3.Lerp(randomVertex, controlGeoCenter, Random.Range(0,1));
+		//randomPointAB = Vector3.Lerp(randomPointAB, currentPoint,.5f);
+
+	}
+	
+	void assignPoint(){
+		a.transform.position = randomPointAB;
+		currentPoint = randomPointAB;
+	}
+
+
+}
---
+++ Assets/_Scripts/MyAudioParticles.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MyAudioParticles : MonoBehaviour {
+
+	public ParticleSystem[] myParticle;
+
+	Vector3 controlGeoCenter;
+
+	ParticleSystem[] a = new ParticleSystem[3];
+
+	void Awake()
+	{
+		//controlGeoCenter = Camera.main.transform.position + new Vector3(0,0,20);
+		controlGeoCenter = GetComponent<Transform>().position;
+		a[0] = (ParticleSystem)Instantiate(myParticle[0], controlGeoCenter, Quaternion.identity);
+		a[0].transform.parent = gameObject.transform;
+		a[1] = (ParticleSystem)Instantiate(myParticle[1], nextPoint(a[0].transform.position), Quaternion.identity);
+		a[1].transform.parent = gameObject.transform;
+		a[2] = (ParticleSystem)Instantiate(myParticle[2], nextPoint(a[1].transform.position), Quaternion.identity);
+		a[2].transform.parent = gameObject.transform;
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		//Update base position of transform 
+		
+		float audioInput = AudioPeer.audioBandBuffer[1];
+
+		if(audioInput>0.7f && audioInput<0.9f ){
+			a[0].transform.position = nextPoint(a[2].transform.position);
+		}
+
+		if(audioInput<0.7f && audioInput>0.6f){
+			a[1].transform.position = nextPoint(a[0].transform.position);
+		}
+
+		if(audioInput<0.5f && audioInput>0.3f){
+			a[2].transform.position = nextPoint(a[1].transform.position);
+		}
+
+
+	}
+
+	
+    private static Vector3 RandomPointInBox(Vector3 center, Vector3 size) {
+ 
+		return center + new Vector3(
+			(Random.value - 0.5f) * size.x,
+			(Random.value - 0.5f) * size.y,
+			(Random.value - 0.5f) * size.z
+		);
+	}
+	
+
+	Vector3 nextPoint(Vector3 currentPoint){
+		return RandomPointInBox(controlGeoCenter, new Vector3 (0,20,20));
+	}
+
+}
