43d44a3c4ebafd49e66eba604cb0229817dd2cbc
AV <av@anastasia.io>
2017-06-13 11:09:26 -0700

    added CSV parser, started audio morph script, added CSV point files

---
+++ Assets/_Scripts/AudioMorph.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioMorph : MonoBehaviour {
+
+	string[] basePositionCSVS; 
+	Vector3[,] basePositions; //Vector3 positions from surface points
+	Vector3[] currentPositions; 
+
+	GameObject[] cubes;
+	int positionIndex;
+
+	int positionVectorCount;
+	
+	// Use this for initialization
+	void Start () {
+		
+		getBasePositionsFromCSVS();
+
+		positionIndex = 0;
+		positionVectorCount = basePositions.GetLength(1);
+		currentPositions = new Vector3[positionVectorCount];
+
+		for (int i= 0; i<positionVectorCount; i++){
+			currentPositions[i] = basePositions[0,i]; 
+		}
+		createCubes(currentPositions);
+		setCubePosition(currentPositions);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		float audioInput = AudioPeer.audioBandBuffer[5];
+
+		if(audioInput>0.9f && audioInput<1f ){
+			setNextPostitions();
+		}
+
+
+		setCubePosition(currentPositions);
+	}
+
+
+
+	void getBasePositionsFromCSVS() {
+		
+		basePositionCSVS = new string[] 
+			{"Points/points-surface-0",
+			"Points/points-surface-1",
+			"Points/points-surface-2", 
+			"Points/points-surface-3"  
+			};
+		
+		for(int i=0; i<basePositionCSVS.Length; i++){
+			
+			TextAsset pointsCSV = Resources.Load(basePositionCSVS[i]) as TextAsset;
+
+			CSVParsing c = new CSVParsing(pointsCSV, 3);
+			string[,] pointsStrings = c.readData();
+
+			if (i==0){
+				basePositions = new Vector3[basePositionCSVS.Length,pointsStrings.GetLength(0)];	
+			}
+
+			//TO-DO - ending up with nulls in the array - need to add some better handl ing
+			for(int j=0; j<pointsStrings.GetLength(0); j++){
+				basePositions[i,j] = new Vector3(float.Parse(pointsStrings[j,0]),float.Parse(pointsStrings[j,1]),float.Parse(pointsStrings[j,2]));
+			}
+		}
+
+	}
+
+	void setNextPostitions(){
+		if(positionIndex==3){
+			positionIndex =0;
+		}
+		else{
+			positionIndex++;
+		}
+		
+		for(int i = 0; i<positionVectorCount; i++){
+			currentPositions[i] = basePositions[positionIndex,i]; 
+		}
+
+	}
+
+	GameObject[] createCubes(Vector3[] position){
+		
+		cubes = new GameObject[position.Length];
+		
+		for(int i =0; i<position.Length; i++){
+			cubes[i]=GameObject.CreatePrimitive(PrimitiveType.Cube);
+		}
+		return cubes;
+	}
+
+	void setCubePosition(Vector3[] position) {
+
+		for(int i =0; i<position.Length; i++){
+			cubes[i].transform.position = position[i];
+		}
+	}
+
+
+
+}
---
+++ Assets/_Scripts/ParseCSV.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+public class CSVParsing
+{
+    public TextAsset csvFile; // Reference of CSV file
+    private char lineSeperater = '\n'; // It defines line seperate character
+    private char fieldSeperator = ','; // It defines field seperate chracter
+
+    private int numCol;
+    public CSVParsing(TextAsset csvFileIn, int numColIn)
+    {
+        csvFile = csvFileIn;
+        numCol = numColIn;
+    }
+
+
+    // Read data from CSV file
+	// TODO: Pass in an array to write to, return an array, or something...
+    public string[,] readData()
+    {
+        string[] records = csvFile.text.Split(lineSeperater);
+        string[,] output = new string[records.Length-1, numCol];
+
+        // Last row is empty due to newline at bottom of csv (so ... -1)
+        for (int i = 0; i < records.Length-1; i++ )
+        {
+            string record = records[i];
+            string[] fields = record.Split(fieldSeperator);
+            
+            for (int j = 0; j < fields.Length; j++ )
+            {
+
+                if (fields.Length != numCol){
+
+                    throw new Exception(String.Format("# of fields in CSV didn't match expected # of columns. Expected: {0}, found: {1}", numCol, fields.Length));
+                    continue;
+                }
+                output[i,j] = fields[j];
+                // Debug.Log(field);
+				// contentArea.text += field + "\t";
+            }
+			// Debug.Log("\n");
+            // contentArea.text += '\n';
+        }
+        return output;
+    }
+    // Get path for given CSV file
+    private static string getPath()
+    {
+		#if UNITY_EDITOR
+				return Application.dataPath;
+		#elif UNITY_ANDROID
+		return Application.persistentDataPath;// +fileName;
+		#elif UNITY_IPHONE
+		return GetiPhoneDocumentsPath();// +"/"+fileName;
+		#else
+		return Application.dataPath;// +"/"+ fileName;
+		#endif
+    }
+	
+    // Get the path in iOS device
+    private static string GetiPhoneDocumentsPath()
+    {
+        string path = Application.dataPath.Substring(0, Application.dataPath.Length - 5);
+        path = path.Substring(0, path.LastIndexOf('/'));
+        return path + "/Documents";
+    }
+}
